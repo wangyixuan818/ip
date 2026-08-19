98600c84cb966470a1df14267b914bebc7872edf
yixuan <wangyixuan818@gmail.com>
2026-08-19 18:34:45 +0800

    added echo feature

--- src/main/java/Noms.java
+++ src/main/java/Noms.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Noms {
     public static void main(String[] args) {
         String banner = "____________________________________________________________\n"
@@ -9,9 +11,24 @@ public class Noms {
                 + "____________________________________________________________";
         System.out.println(banner);
         System.out.println("Hello! I'm Noms.");
-        System.out.println("What can I do for you?");
+        System.out.println("NomNom, have you eaten? What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String command = scanner.nextLine();
+
+            System.out.println(" " + command);
+            System.out.println("____________________________________________________________");
+
+            if (command.equals("bye")) {
+                System.out.println("Bye~ Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+        }
+
+        scanner.close();
     }
 }
