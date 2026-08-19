14595094dec52d16801b2b66edc74604f03fcf50
yixuan <wangyixuan818@gmail.com>
2026-08-19 18:22:38 +0800

    updated the bot name to Noms

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Noms User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Noms.java
@@ -0,0 +1,10 @@
+public class Noms {
+    public static void main(String[] args) {
+        String banner = " _   _  ___  __  __  ____\n"
+                + "| \\ | |/ _ \\|  \\/  |/ ___|\n"
+                + "|  \\| | | | | |\\/| |\\___ \\\n"
+                + "| |\\  | |_| | |  | | ___) |\n"
+                + "|_| \\_|\\___/|_|  |_||____/\n";
+        System.out.println(banner);
+    }
+}
