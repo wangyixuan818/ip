413124c8b7d8b40a48e87e64f1cbbc15abef1fdd
yixuan <wangyixuan818@gmail.com>
2026-08-19 18:25:34 +0800

    updated hello and exit msg

--- src/main/java/Noms.java
+++ src/main/java/Noms.java
@@ -1,10 +1,17 @@
 public class Noms {
     public static void main(String[] args) {
-        String banner = " _   _  ___  __  __  ____\n"
+        String banner = "____________________________________________________________\n"
+                + " _   _  ___  __  __  ____\n"
                 + "| \\ | |/ _ \\|  \\/  |/ ___|\n"
-                + "|  \\| | | | | |\\/| |\\___ \\\n"
-                + "| |\\  | |_| | |  | | ___) |\n"
-                + "|_| \\_|\\___/|_|  |_||____/\n";
+                + "|  \\| | | | | |\\/| | \\___ \\\n"
+                + "| |\\  | |_| | |  | |  ___) |\n"
+                + "|_| \\_|\\___/|_|  |_| |____/\n"
+                + "____________________________________________________________";
         System.out.println(banner);
+        System.out.println("Hello! I'm Noms.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
